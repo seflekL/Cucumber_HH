ebb9f36ef46a6903a773e6b47092a55df43413e5
Levent SEFLEK <leventseflek@gmail.com>
2024-09-23 22:38:05 +0300

    auto

--- src/test/java/stepdefinitions/test1.java
+++ src/test/java/stepdefinitions/test1.java
@@ -5,6 +5,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hellowworld");
         System.out.println("hellowworld");
+        System.out.println("hellowworld");
     }
 
 
